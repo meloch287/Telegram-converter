64c04ef21a13a427362560201c544e69593550bc
AleksandrMaksimenko <123982407+meloch287@users.noreply.github.com>
2024-11-28 03:31:05 +0300

    Update run.py

--- run.py
+++ run.py
@@ -3,7 +3,7 @@ from aiogram import Bot, Dispatcher, F
 from handlers import router
 
 async def main():
-    bot = Bot(token='7525690138:AAFdojSGaRqL3idPaO_7hJsd_NpcH9aeic8')
+    bot = Bot(token='-Заглушка-')
     dp = Dispatcher()
     dp.include_router(router)
     await dp.start_polling(bot)
